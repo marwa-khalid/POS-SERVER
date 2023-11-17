106419370ab205d70d9eebfa48e75a72e63336d4
Marwa <marwakhalid558@gmail.com>
2023-11-17 18:33:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 const userRoute = require("./Routes/User");
 const productRoute = require("./Routes/Product");
 const orderRoute = require("./Routes/Order");
@@ -12,20 +19,11 @@ const salaryRoute = require("./Routes/Salary");
 
 const DB = "mongodb://marwakhalid:marwamarwa@marwa-shard-00-00.x9zjp.mongodb.net:27017,marwa-shard-00-01.x9zjp.mongodb.net:27017,marwa-shard-00-02.x9zjp.mongodb.net:27017/POS?ssl=true&replicaSet=atlas-6szo5v-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.use(cors({
-    origin: ["https://pos-system-api-hasnans-projects.vercel.app/"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true
-}));
-
 mongoose.set("strictQuery", false);
 mongoose.connect(DB,{}).then(()=>{
     console.log("connection successful");
 }).catch((err)=>console.log("no connection"));
 
-app.use(express.json());
 
 app.use("/api/user",userRoute);
 app.use("/api/product",productRoute);
@@ -33,6 +31,6 @@ app.use("/api/order",orderRoute);
 app.use("/api/purchase",purchaseRoute);
 app.use("/api/salary",salaryRoute);
 
-app.listen(8201, ()=>{
+app.listen(3000, ()=>{
     console.log("backened is running");
 });
